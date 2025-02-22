d94d74210f73fdba3137a855794e70d51e8432de
hms0725 <hms910725@naver.com>
2025-02-22 22:56:20 +0900

    d

--- src/main/java/com/example/reactRemote/dao/ReactremoteDao.java
+++ src/main/java/com/example/reactRemote/dao/ReactremoteDao.java
@@ -1,5 +1,17 @@
 package com.example.reactRemote.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
 public class ReactremoteDao {
+	
+	private final SqlSession session;
+	
+	
+	
 
 }
--- src/main/java/com/example/reactRemote/service/ReactremoteService.java
+++ src/main/java/com/example/reactRemote/service/ReactremoteService.java
@@ -1,5 +1,8 @@
 package com.example.reactRemote.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReactremoteService {
 
 }
